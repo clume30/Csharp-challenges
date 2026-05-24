682708a9e7981096e681b7765a4836c2c2cf583b
clume30 <umeiheukwulawrence@gmail.com>
2026-05-24 01:22:58 +0100

    Add files via upload
    Here I learnt to use the Try.Parse function

---
+++ challenge7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace challenge7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? userInput = null;
+            int result = 0;
+            Console.WriteLine("Enter an integer between 5 and 10");
+            while (!(result > 5 && result < 10))
+            {
+                userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out result))
+                {
+                    Console.WriteLine("you have successfully input an integer");
+                }
+                else
+                {
+                    Console.WriteLine("invalid input, Please enter whole numbers\n\n try again");
+                    continue;
+                }
+                // testing the whole number if it reeached the requirements
+                if (result > 5 && result < 10)
+                {
+                    Console.WriteLine("we have received your request");
+                }
+                else
+                    Console.WriteLine("whole number must be greater than 5 and less than 10!!!\n\n try again");
+                    continue;
+            }
+            
+        }
+    }
+}
