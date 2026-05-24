e668cd6c33ab577afebf0b2467657d967b97a008
clume30 <umeiheukwulawrence@gmail.com>
2026-05-24 01:02:49 +0100

    Add files via upload
    A C# program that uses the Math class

---
+++ challenge1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace firstChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+            Console.WriteLine($"The maximum number is: {numbers.Max()}" );
+            Console.WriteLine($"The minimum number is: {numbers.Min()}");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(Math.Sqrt(numbers[i]));
+            }
+        }
+    }
+}
